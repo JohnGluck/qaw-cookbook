5b2e2bf644db9508578e3fb848149216464da65d
John Gluck <john@qawolf.com>
2025-03-23 16:45:41 -0700

    blob reporting

--- playwright.config.recipe3.ts
+++ playwright.config.recipe3.ts
@@ -23,13 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['json', {
-      outputFile: `test-results/results-${process.env.BROWSER || 'default'}-${process.env.SHARD || '0'}.json`
-    }],
-    ['html', {
-      outputFolder: `test-results/results-${process.env.BROWSER || 'default'}-${process.env.SHARD || '0'}`,
-      open: 'never'
-    }]
+    ['blob'],
   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
