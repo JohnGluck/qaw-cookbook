ccbbfb5e2b08c08aa6090ccf08d9ee6b48f24492
John Gluck <john@qawolf.com>
2025-03-23 15:37:33 -0700

    fix error in config

--- playwright.config.recipe3.ts
+++ playwright.config.recipe3.ts
@@ -30,7 +30,7 @@ export default defineConfig({
       outputFolder: `test-results/results-${process.env.BROWSER || 'default'}-${process.env.SHARD || '0'}`,
       open: 'never'
     }]
-  ]
+  ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
