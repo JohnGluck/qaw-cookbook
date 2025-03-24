1855a4a44676162600b2bfe91eb3a363841de8ec
John Gluck <john@qawolf.com>
2025-03-23 17:19:14 -0700

    cleanup

--- playwright.config.recipe3.ts
+++ playwright.config.recipe3.ts
@@ -23,9 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['blob', { outputDir: 'blob-results' }]
+    ['json', { outputFile: 'test-results/report.json' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
-
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
