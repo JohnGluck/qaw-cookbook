42f118d792d9ad1c190660cfb365bcadce7dd758
John Gluck <john@qawolf.com>
2025-03-26 20:37:05 -0700

    port binding

--- argo-worflow-setup.sh
+++ argo-worflow-setup.sh
@@ -1,7 +1,12 @@
+#/bin/bash
+#assumes you have k8s,kubectl, docker and helm installed.  Rancher is cool
+docker build -t localhost:5000/playwright-tests:latest .
+docker push localhost:5000/playwright-tests:latest
 kubectl create namespace argo
 helm install argo argo/argo-workflows --namespace argo     --set server.extraArgs[0]="--auth-mode=server"
-kubectl apply -f .argo/infrastructure/rbac.yml 
-kubectl apply -f .argo/infrastructure/service_account.yml 
+kubectl apply -f .argo/infrastructure/rbac.yml
+kubectl apply -f .argo/infrastructure/service_account.yml
+kubectl port-forward svc/argo-argo-workflows-server -n argo 2746:8080
 kubectl create serviceaccount argo-client -n argo
 kubectl create rolebinding argo-client-admin --clusterrole=admin --serviceaccount=argo:argo-client -n argo
 SECRET_NAME=$(kubectl get serviceaccount argo-client -n argo -o jsonpath='{.secrets[0].name}')
