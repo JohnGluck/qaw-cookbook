09351114b827dde4d1454584f91386ee21c18da7
John Gluck <john@qawolf.com>
2025-03-27 12:21:17 -0700

    Trying the whole job

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+      ['blob'],
       ['list'],
       ['html', { outputFolder: '/tmp/playwright-report', open: 'never' }]
     ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
