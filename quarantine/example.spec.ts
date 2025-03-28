aa2d7c3afe1a19fb048cbf79655215dd656c924b
John Gluck <john@qawolf.com>
2025-03-27 17:45:40 -0700

    modifying quarantine tests so they fail faster

--- quarantine/example.spec.ts
+++ quarantine/example.spec.ts
@@ -11,8 +11,8 @@ test('get started link', { tag: ['@regression'] }, async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'This should break' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Break' })).toBeVisible();
 });
